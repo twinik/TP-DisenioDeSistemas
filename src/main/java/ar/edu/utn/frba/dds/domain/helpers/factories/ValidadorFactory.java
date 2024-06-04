b911acb8817221d515bbdc0b23ef12d9d12572d9
Gonzalo Ivo Turri <gturri@frba.utn.edu.ar>
2024-06-04 13:06:01 -0300

    correccion del factory del validador

--- src/main/java/ar/edu/utn/frba/dds/domain/helpers/factories/ValidadorFactory.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/helpers/factories/ValidadorFactory.java
@@ -1,10 +1,25 @@
 package ar.edu.utn.frba.dds.domain.helpers.factories;
 
 import ar.edu.utn.frba.dds.domain.helpers.ValidadorClaves;
+import ar.edu.utn.frba.dds.domain.helpers.validaciones.Validacion;
+import ar.edu.utn.frba.dds.domain.helpers.validaciones.ValidacionLargoClaves;
+import ar.edu.utn.frba.dds.domain.helpers.validaciones.ValidacionListaClavesNuevo;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * ValidadorFactory interface permite crear un validador de claves.
  */
-public interface ValidadorFactory {
-  ValidadorClaves crearValidador();
+public class ValidadorFactory {
+
+  /**
+   *
+   * @return un validador de claves configurado segun el estandar owasp
+   */
+  public static ValidadorClaves create() {
+    List<Validacion> estrategias = new ArrayList<>();
+    estrategias.add(new ValidacionLargoClaves());
+    estrategias.add(new ValidacionListaClavesNuevo());
+    return new ValidadorClaves(estrategias);
+  }
 }
