1f57d137a93033e377321306f50ef9255ac760cc
uriGrif <urielgrif@gmail.com>
2024-08-27 16:32:11 -0300

    agrego script start a api e ignoro /dist

--- recomendador-donacion-api/src/controllers/donacionController.ts
+++ recomendador-donacion-api/src/controllers/donacionController.ts
@@ -3,36 +3,40 @@ import { obtenerComunidades } from "../services/comunidadService";
 import { calcularDistancia } from "../utils/distanceCalculator";
 
 export function obtenerLugares(req: Request, res: Response) {
-  const lat = parseFloat(req.query.lat as string);
-  const lon = parseFloat(req.query.lon as string);
-  const limite = parseInt(req.query.limite as string) || 5;
-  const distanciaMaxEnKM =
-    parseFloat(req.query.distanciaMaxEnKM as string) || 2;
+	const lat = parseFloat(req.query.lat as string);
+	const lon = parseFloat(req.query.lon as string);
+	const limite = parseInt(req.query.limite as string) || 5;
+	const distanciaMaxEnKM =
+		parseFloat(req.query.distanciaMaxEnKM as string) || 0;
 
-  if (isNaN(lat) || isNaN(lon)) {
-    return res
-      .status(400)
-      .json({ error: "Faltan parámetros de latitud o longitud válidos" });
-  }
+	if (isNaN(lat) || isNaN(lon)) {
+		return res
+			.status(400)
+			.json({ error: "Faltan parámetros de latitud o longitud válidos" });
+	}
 
-  const comunidades = obtenerComunidades();
+	const comunidades = obtenerComunidades();
 
-  const lugares = comunidades
-    .map((comunidad) => {
-      const distancia = calcularDistancia(
-        lat,
-        lon,
-        comunidad.lat,
-        comunidad.lon
-      );
-      return {
-        ...comunidad,
-        distanciaEnKM: parseFloat(distancia.toFixed(2)),
-      };
-    })
-    .sort((a, b) => a.distanciaEnKM - b.distanciaEnKM)
-    .filter((comunidad) => comunidad.distanciaEnKM <= distanciaMaxEnKM)
-    .slice(0, limite);
+	const lugares = comunidades
+		.map(comunidad => {
+			const distancia = calcularDistancia(
+				lat,
+				lon,
+				comunidad.lat,
+				comunidad.lon
+			);
+			return {
+				...comunidad,
+				distanciaEnKM: parseFloat(distancia.toFixed(2))
+			};
+		})
+		.filter(comunidad =>
+			distanciaMaxEnKM == 0
+				? true
+				: comunidad.distanciaEnKM <= distanciaMaxEnKM
+		)
+		.sort((a, b) => a.distanciaEnKM - b.distanciaEnKM)
+		.slice(0, limite);
 
-  res.json({ lugares });
+	res.json({ lugares });
 }
