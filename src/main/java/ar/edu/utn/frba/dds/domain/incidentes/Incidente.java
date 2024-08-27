8af111ac12c371e484aa6072f7c30e06e7a32458
Gonzalo Ivo Turri <gturri@frba.utn.edu.ar>
2024-08-27 10:00:46 -0300

    actualizar nombre DTYPE

--- src/main/java/ar/edu/utn/frba/dds/domain/incidentes/Incidente.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/incidentes/Incidente.java
@@ -12,6 +12,8 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import javax.persistence.Column;
 import javax.persistence.Convert;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -20,6 +22,7 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Transient;
 import java.time.LocalDateTime;
 
@@ -31,6 +34,8 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Table(name = "incidente")
+@DiscriminatorColumn(name = "tipo_incidente")
 public class Incidente {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
