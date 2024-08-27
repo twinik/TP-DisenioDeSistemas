7282556a7b1b1ef9540c6dc9ae8bc68c79997a36
noteloolvidesabierto <MMartinez28124@gmail.com>
2024-08-27 16:21:49 -0300

    persitencia altaPersonaVulnerableRepository

--- src/main/java/ar/edu/utn/frba/dds/repositories/imp/AlertasRepository.java
+++ src/main/java/ar/edu/utn/frba/dds/repositories/imp/AlertasRepository.java
@@ -40,3 +40,73 @@ public class AlertasRepository implements IAlertasRepository {
 
   }
 }
+/*public class AlertasRepository implements IAlertasRepository, WithSimplePersistenceUnit {
+
+  private List<Alerta> alertas;
+
+  public AlertasRepository() {
+    alertas = new ArrayList<>();
+  }
+
+
+  @Override
+  public Optional<Alerta> buscar(long id) { //Alerta no tiene ID, ver que se hace
+   return Optional.ofNullable(entityManager().find(Alerta.class,id));
+  }
+
+  @Override
+  public List<Alerta> buscarTodos() {
+    return entityManager().createQuery("from Alerta",Alerta.class).getResultList();
+  }
+
+  @Override
+  public void guardar(Alerta alerta) {
+    withTransaction(() -> entityManager().persist(alerta));
+  }
+
+  public void guardar(Alerta ...alerta) {
+
+    withTransaction(() -> {
+      for (Alerta tipoAlerta : alerta){
+        entityManager().persist(tipoAlerta);
+      }
+    });
+  }
+
+  @Override
+  public void actualizar(Alerta alerta) {
+    withTransaction(() -> entityManager().merge(alerta));
+  }
+
+  @Override
+  public void eliminar(Alerta alerta) {
+    //alerta.setActivo(false);
+    withTransaction(() -> entityManager().merge(alerta));
+  }
+
+  public static void main(String[] args) {
+    Alerta m = new Alerta(TipoAlerta.TEMPERATURA);
+    Alerta m1 = new Alerta(TipoAlerta.FRAUDE);
+    Alerta m2 = new Alerta(TipoAlerta.FALLA_CONEXION);
+    IAlertasRepository repositorio = (IAlertasRepository) ServiceLocator.get("alertasRepository");
+    repositorio.guardar(m);
+    repositorio.guardar(m1);
+    repositorio.guardar(m2);
+
+    repositorio.eliminar(m1);
+    try {
+      Thread.sleep(60 * 1000);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+    repositorio.actualizar(m2);
+
+    Optional<Alerta> alerta1 = repositorio.buscar(1L); //Alerta no tiene ID, ver que se hace
+    //System.out.println(hidratado.get().getMotivo());
+    Optional<Alerta> alerta2 = repositorio.buscar(2L); //Alerta no tiene ID, ver que se hace
+
+    List<Alerta> lista = repositorio.buscarTodos();
+
+  }
+
+}*/
