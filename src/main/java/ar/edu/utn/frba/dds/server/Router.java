74b82143d8cef821c139f6a76d09bd73b9f56f2c
twinik <tobiwinik@gmail.com>
2024-09-26 14:43:58 -0300

    server and utils

--- src/main/java/ar/edu/utn/frba/dds/server/Router.java
+++ src/main/java/ar/edu/utn/frba/dds/server/Router.java
@@ -1,4 +1,11 @@
 package ar.edu.utn.frba.dds.server;
 
+import io.javalin.Javalin;
+
 public class Router {
+  public static void init(Javalin app) {
+    app.get("/", ctx -> {
+      ctx.result("Hello World");
+    });
+  }
 }
---
+++ src/main/java/ar/edu/utn/frba/dds/utils/Initializer.java
@@ -0,0 +1,8 @@
+package ar.edu.utn.frba.dds.utils;
+
+public class Initializer {
+
+  public static void init() {
+    // TODO
+  }
+}
