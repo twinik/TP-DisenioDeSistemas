27ceaf3df79de245c4abe5f0022736c3c6cbd72d
uriGrif <urielgrif@gmail.com>
2024-10-09 18:57:49 -0300

    listo los formularios y las respuestas. Falta unirlo al flujo del registro y arreglar el css q se ve nefasto

--- src/main/java/ar/edu/utn/frba/dds/controllers/RespuestaFormularioController.java
+++ src/main/java/ar/edu/utn/frba/dds/controllers/RespuestaFormularioController.java
@@ -1,6 +1,8 @@
 package ar.edu.utn.frba.dds.controllers;
 
+import ar.edu.utn.frba.dds.dtos.formularios.RespuestaFormularioDto;
 import ar.edu.utn.frba.dds.dtos.formularios.ShowFormularioDto;
+import ar.edu.utn.frba.dds.exceptions.NoAutorizadoException;
 import ar.edu.utn.frba.dds.services.FormulariosService;
 import ar.edu.utn.frba.dds.services.RespuestaFormularioService;
 import ar.edu.utn.frba.dds.utils.ICrudViewsHandler;
@@ -27,7 +29,9 @@ public class RespuestaFormularioController implements ICrudViewsHandler {
 
   @Override
   public void create(Context context) {
-    // TODO chequear que es el colaborador
+    if (context.sessionAttribute("idColaborador") != context.pathParam("idColaborador")) {
+      throw new NoAutorizadoException("No estas autorizado para responder este formulario");
+    }
     Map<String, Object> model = new HashMap<>();
     model.put("formulario", ShowFormularioDto.fromFormulario(formService.obtenerFormulario(context.pathParam("idFormulario"))));
     context.render("auth/registro/formulario-colaborador.hbs", model);
@@ -35,7 +39,10 @@ public class RespuestaFormularioController implements ICrudViewsHandler {
 
   @Override
   public void save(Context context) {
-
+    rtaService.crearRespuestaFormulario(RespuestaFormularioDto.fromContext(context));
+    Map<String, Object> model = new HashMap<>();
+    model.put("message", "Su respuesta fue guardad con exito! Gracias!");
+    context.render("app/success.hbs", model);
   }
 
   @Override
