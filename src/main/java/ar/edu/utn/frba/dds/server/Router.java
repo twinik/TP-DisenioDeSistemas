facfb733c47c5982a1eb7c442f93aa5be3d8616a
Thomas Luca <tluca@frba.utn.edu.ar>
2024-10-10 12:10:57 -0300

    Alta de tarjetas VIEW terminada

--- src/main/java/ar/edu/utn/frba/dds/server/Router.java
+++ src/main/java/ar/edu/utn/frba/dds/server/Router.java
@@ -41,6 +41,7 @@ public class Router {
     app.post("/admin/formularios/nuevo", ServiceLocator.get(FormulariosController.class)::save);
     app.get("/admin/modelos-heladeras/nuevo", ServiceLocator.get(ModelosHeladeraController.class)::create);
     app.post("/admin/modelos-heladeras/nuevo", ServiceLocator.get(ModelosHeladeraController.class)::save);
+    app.get("/admin/tarjetas/nuevo", ctx -> ctx.render("/app/admin/alta-tarjetas.hbs"));  //TODO hacerle el controller
 
     app.get("/responder-formulario/{idFormulario}/colaborador/{idColaborador}", ServiceLocator.get(RespuestaFormularioController.class)::create);
     app.post("/responder-formulario/{idFormulario}/colaborador/{idColaborador}", ServiceLocator.get(RespuestaFormularioController.class)::save);
