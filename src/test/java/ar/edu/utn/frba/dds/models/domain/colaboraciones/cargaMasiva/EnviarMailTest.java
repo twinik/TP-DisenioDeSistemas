b4e5baf7210ee2e4727e5e5065c24e3b01b0d47e
twinik <tobiwinik@gmail.com>
2024-10-11 10:19:18 -0300

    fix sendgrid

---
+++ src/test/java/ar/edu/utn/frba/dds/models/domain/colaboraciones/cargaMasiva/EnviarMailTest.java
@@ -0,0 +1,32 @@
+package ar.edu.utn.frba.dds.models.domain.colaboraciones.cargaMasiva;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import ar.edu.utn.frba.dds.models.domain.emailSending.MyEmail;
+import ar.edu.utn.frba.dds.models.domain.emailSending.SendGridMailSender;
+import java.io.IOException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EnviarMailTest {
+  private SendGridMailSender mailSender;
+
+  @BeforeEach
+  void setUp() throws IOException {
+    this.mailSender = new SendGridMailSender();
+  }
+
+  @Test
+  void enviarMail() {
+    MyEmail email = MyEmail
+        .builder()
+        .emisor("grupo7ddsutn@gmail.com")
+        .receptor("gturri@frba.utn.edu.ar")
+        .asunto("Test")
+        .cuerpo("Test SendGrid")
+        .build();
+
+    this.mailSender.enviarMail(email);
+    assertDoesNotThrow(() -> this.mailSender.enviarMail(email));
+  }
+}
