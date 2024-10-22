f660409218561584cb0dc6ef4c96712515e85ac3
Thomas Luca <tluca@frba.utn.edu.ar>
2024-10-22 20:28:27 -0300

    piso

--- src/main/java/ar/edu/utn/frba/dds/dtos/DireccionDto.java
+++ src/main/java/ar/edu/utn/frba/dds/dtos/DireccionDto.java
@@ -20,7 +20,7 @@ public class DireccionDto {
         .builder()
         .calle(context.formParam("calle"))
         .altura(Integer.parseInt(context.formParam("altura")))
-        .piso( (context.formParam("piso") != null && !context.formParam("pisio").isBlank()) ? Integer.parseInt(context.formParam("piso")) : null)
+        .piso( (context.formParam("piso") != null && !context.formParam("piso").isBlank()) ? Integer.parseInt(context.formParam("piso")) : null)
         .codigoPostal(context.formParam("codigoPostal"))
         .build();
   }
