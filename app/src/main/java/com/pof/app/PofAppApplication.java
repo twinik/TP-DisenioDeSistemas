8af8c127455678da2f21ad970f9bd0f844c51e00
twinik <tobiwinik@gmail.com>
2024-11-11 14:12:32 -0300

    init pof

---
+++ app/src/main/java/com/pof/app/PofAppApplication.java
@@ -0,0 +1,13 @@
+package com.pof.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PofAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PofAppApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/pof/app/PofAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pof.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PofAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
