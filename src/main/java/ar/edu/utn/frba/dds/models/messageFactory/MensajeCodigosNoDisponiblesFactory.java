b755c92b010d13694a4608851c3343a53172b3d3
Gonzalo Ivo Turri <gturri@frba.utn.edu.ar>
2024-11-19 22:17:46 -0300

    fix typo

--- src/main/java/ar/edu/utn/frba/dds/models/messageFactory/MensajeCodigosNoDisponiblesFactory.java
+++ src/main/java/ar/edu/utn/frba/dds/models/messageFactory/MensajeCodigosNoDisponiblesFactory.java
@@ -2,6 +2,6 @@ package ar.edu.utn.frba.dds.models.messageFactory;
 
 public class MensajeCodigosNoDisponiblesFactory {
   public static String generarMensaje() {
-    return "No hay tarjetas disponbiles intentelo mas tarde";
+    return "No hay tarjetas disponibles inténtelo mas tarde";
   }
 }
